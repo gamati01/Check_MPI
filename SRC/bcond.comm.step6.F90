63cf52661383241b7504e046bd777b553a58caaf
GIORGIO AMATI <gamati01@dgxslurm.m100.cineca.it>
2023-12-23 01:06:44 +0100

    Fix step6

--- SRC/bcond.comm.step6.F90
+++ SRC/bcond.comm.step6.F90
@@ -44,8 +44,10 @@
         real(mystor), dimension(:,:,:), allocatable, save :: bufferYOUTP
         real(mystor), dimension(:,:,:), allocatable, save :: bufferYOUTM
 !        
-        real(mystor), dimension(:,:,:), allocatable, save :: bufferZIN
-        real(mystor), dimension(:,:,:), allocatable, save :: bufferZOUT
+        real(mystor), dimension(:,:,:), allocatable, save :: bufferZINP
+        real(mystor), dimension(:,:,:), allocatable, save :: bufferZINM
+        real(mystor), dimension(:,:,:), allocatable, save :: bufferZOUTP
+        real(mystor), dimension(:,:,:), allocatable, save :: bufferZOUTM
 !
         integer      :: status_front(MPI_STATUS_SIZE)
         integer      :: status_rear(MPI_STATUS_SIZE)
@@ -56,6 +58,11 @@
         integer      :: status_left(MPI_STATUS_SIZE)
         integer      :: reqs_right(1)
         integer      :: reqs_left(1)
+!        
+        integer      :: status_up(MPI_STATUS_SIZE)
+        integer      :: status_down(MPI_STATUS_SIZE)
+        integer      :: reqs_up(1)
+        integer      :: reqs_down(1)
 !        
 ! start timing...
         call SYSTEM_CLOCK(countA0, count_rate, count_max)
@@ -72,8 +79,10 @@
            allocate(bufferYOUTP(0:l+1,0:n+1,1:3))
            allocate(bufferYOUTM(0:l+1,0:n+1,1:3))
 !           
-           allocate(bufferZIN (0:l+1,0:m+1,1:3))
-           allocate(bufferZOUT(0:l+1,0:m+1,1:3))
+           allocate(bufferZINP (0:l+1,0:m+1,1:3))
+           allocate(bufferZINM (0:l+1,0:m+1,1:3))
+           allocate(bufferZOUTP(0:l+1,0:m+1,1:3))
+           allocate(bufferZOUTM(0:l+1,0:m+1,1:3))
         endif
 !        
         msgsizeX = (n+2)*(m+2)*3
@@ -84,7 +93,9 @@
 ! optimization but also because CUDA-aware MPI_Sendrecv uses inefficient
 ! H2D + D2H copies
 !        
-!$acc enter data create(bufferXINP,bufferXINM,bufferXOUTM,bufferXOUTP,bufferYINP,bufferYINM,bufferYOUTP,bufferYOUTM,bufferZIN,bufferZOUT)
+!$acc enter data create(bufferXINP,bufferXINM,bufferXOUTM,bufferXOUTP & 
+!$acc&                 ,bufferYINP,bufferYINM,bufferYOUTP,bufferYOUTM &
+!$acc&                 ,bufferZINP,bufferZINM,bufferZOUTP,bufferZOUTM)
 !
 !------------------------------------------------------------------------
 ! comms along z + 
@@ -116,66 +127,66 @@
 !                
            call time(tcountZ0)
 !           
+! First pack data.....                
+!$acc kernels
+           do j = 0,m+1
+              do i = 0,l+1
+                 bufferZINP(i,j,1)=field1(i,j,n)
+                 bufferZINP(i,j,2)=field2(i,j,n)
+                 bufferZINP(i,j,3)=field3(i,j,n)
+!
+                 bufferZINM(i,j,1)=field1(i,j,1)
+                 bufferZINM(i,j,2)=field2(i,j,1)
+                 bufferZINM(i,j,3)=field3(i,j,1)
+              enddo
+           enddo
+!$acc end kernels
+!
+! Second send data.....                
            tag = 34
-
-!$acc kernels
-           do j = 0,m+1
-              do i = 0,l+1
-                 bufferZIN(i,j,1)=field1(i,j,n)
-                 bufferZIN(i,j,2)=field2(i,j,n)
-                 bufferZIN(i,j,3)=field3(i,j,n)
-              enddo
-           enddo
-!$acc end kernels
-!
-!$acc host_data use_device(bufferZIN,bufferZOUT)
-           call mpi_sendrecv(bufferZIN(0,0,1),msgsizeZ,MYMPIREAL,up(2),tag,&
-                             bufferZOUT(0,0,1),msgsizeZ,MYMPIREAL,down(2),tag,&
-                             lbecomm,status,ierr)
+!$acc host_data use_device(bufferZINP)
+           call mpi_isend(bufferZINP(0,0,1), msgsizez, MYMPIREAL, up(2), tag, &
+                             lbecomm, reqs_up(1), ierr)
 !$acc end host_data
-!
-!$acc kernels
-           do j = 0,m+1
-              do i = 0,l+1
-                 field1(i,j,0)=bufferZOUT(i,j,1)
-                 field2(i,j,0)=bufferZOUT(i,j,2)
-                 field3(i,j,0)=bufferZOUT(i,j,3)
-              enddo
-           enddo
-!$acc end kernels
-!        
-!           call mpi_barrier(lbecomm,ierr)
-!
-! comms along z - 
 !
            tag = 32
-!
-!$acc kernels
-           do j = 0,m+1
-              do i = 0,l+1
-                 bufferZIN(i,j,1)=field1(i,j,1)
-                 bufferZIN(i,j,2)=field2(i,j,1)
-                 bufferZIN(i,j,3)=field3(i,j,1)
-              enddo
-           enddo
-!$acc end kernels
-!        
-!$acc host_data use_device(bufferZIN,bufferZOUT)
-           call mpi_sendrecv(bufferZIN(0,0,1),msgsizeZ,MYMPIREAL,down(2),tag,&
-                             bufferZOUT(0,0,1),msgsizeZ,MYMPIREAL,up(2),tag,&
-                             lbecomm,status,ierr)
+!$acc host_data use_device(bufferZINM)
+           call mpi_isend(bufferZINM(0,0,1), msgsizez, MYMPIREAL, down(2), tag, &
+                             lbecomm, reqs_down(1), ierr)
 !$acc end host_data
 !
+! Third receive data
+           tag = 34
+!$acc host_data use_device(bufferZOUTP)
+           call mpi_recv(bufferZOUTP(0,0,1), msgsizez, MYMPIREAL, down(2), tag, &
+                             lbecomm, status_up, ierr)
+!$acc end host_data
+!
+           tag = 32
+!$acc host_data use_device(bufferZOUTM)
+           call mpi_recv(bufferZOUTM(0,0,1), msgsizez, MYMPIREAL, up(2), tag, &
+                             lbecomm, status_down, ierr)
+!$acc end host_data
+!
+! Fourth unpack data
 !$acc kernels
            do j = 0,m+1
               do i = 0,l+1
-                 field1(i,j,n+1) = bufferZOUT(i,j,1)
-                 field2(i,j,n+1) = bufferZOUT(i,j,2)
-                 field3(i,j,n+1) = bufferZOUT(i,j,3)
+                 field1(i,j,0)=bufferZOUTP(i,j,1)
+                 field2(i,j,0)=bufferZOUTP(i,j,2)
+                 field3(i,j,0)=bufferZOUTP(i,j,3)
+!
+                 field1(i,j,n+1) = bufferZOUTM(i,j,1)
+                 field2(i,j,n+1) = bufferZOUTM(i,j,2)
+                 field3(i,j,n+1) = bufferZOUTM(i,j,3)
               enddo
            enddo
 !$acc end kernels
 !
+! Fifth  wait...           
+           call mpi_wait(reqs_up(1), status_up, ierr)
+           call mpi_wait(reqs_down(1), status_down, ierr)
+!           
            call time(tcountZ1)
            timeZ = timeZ + (tcountZ1 -tcountZ0)
 !
@@ -243,6 +254,7 @@
 !$acc end host_data
 !
            tag = 11
+!           
 !Third receive data
 !$acc host_data use_device(bufferXOUTP)
            call mpi_recv(bufferXOUTP(0,0,1),msgsizeX,MYMPIREAL,rear(2), tag, &
@@ -255,11 +267,11 @@
                              lbecomm, status_rear, ierr)
 !$acc end host_data
 !
-! Fourth  unpack data
+! Fourth unpack data
 !$acc kernels
            do k = 0,n+1
               do j = 0,m+1
-! x- direction                 
+! x+ direction                 
                  field1(0,j,k) = bufferXOUTP(j,k,1)
                  field2(0,j,k) = bufferXOUTP(j,k,2)
                  field3(0,j,k) = bufferXOUTP(j,k,3)
